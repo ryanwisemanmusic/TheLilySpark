a215e52961bace8b9d38110e40a83f83915c861c
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2025-07-06 18:07:15 -0700

    window_probe.h
    While barebones, these are the functions for opening a window. This needs to be modified as I develop this project further, as it may take months to reach that goal

---
+++ include/window_probe.h
@@ -0,0 +1,12 @@
+#ifndef WINDOW_PROBE_H
+#define WINDOW_PROBE_H
+#include <stdio.h>
+//Generate window attempt
+int window_probe_attempt(const char *device, int width, int height, FILE *log);
+
+//Open window attempt
+void window_probe_bruteforce(const char **devices, int num_devices, int width, int height, const char *logfile);
+
+//Find the right files to open window attempt
+int enumerate_dev_candidates(char candidates[][256], int max_candidates);
+#endif
